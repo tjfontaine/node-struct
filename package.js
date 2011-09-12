5690aaab13f931e39881e1d51e8778e567e09244
Timothy J Fontaine <tjfontaine@gmail.com>
2011-09-12 19:48:53 -0400

    add some npm packaging

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "struct",
+  "version": "0.0.1",
+  "description": "Buffer based implementation of python's struct module",
+  "keywords": [
+    "struct",
+    "python"
+  ],
+  "homepage": "http://github.com/tjfontaine/node-struct",
+  "main": "struct.js",
+}
